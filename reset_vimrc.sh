780277f69b7fa1023d30fded07588dbbb38ba263
efischer19 <efischer0119@gmail.com>
2016-01-23 10:14:23 -0500

    simplify/update script to match las commit

--- reset_vimrc.sh
+++ reset_vimrc.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-rm ~/.vimrc
-rm -rf ~/.vim_runtime
-rm -rf ~/.vim
-cp  copy_to_home/.vimrc ~/
-cp -r copy_to_home/.vim_runtime ~/
-cp -r copy_to_home/.vim ~/
+rm -rf ~/.vim*
+cp -r .vim* ~/
